65053ae44ec47fe6f18c0cc0d05d00a3d176d5a5
Kevin Mora <kambetacode@gmail.com>
2023-08-18 18:25:09 -0500

    Added some content to the DOM with Javascript
    Added a p element with red color
    Added a h3 element with blue color

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const para = document.createElement('p')
+para.classList.add('para');
+para.textContent = "Hey I'm red";
+para.style.color = 'red'
+
+content.appendChild(para);
+
+const title3 = document.createElement('h3')
+title3.classList.add('title3');
+title3.textContent = "I'm a blue h3!";
+title3.style.color = 'blue';
+
+content.appendChild(title3);
